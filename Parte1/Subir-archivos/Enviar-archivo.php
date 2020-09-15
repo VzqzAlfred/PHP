5e5658d142fbd157123a2f2911e69838ff84109e
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-09-15 12:57:44 -0500

    Clase 4: Subir Archivos al Servidor con PHP

--- Parte1/Subir-archivos/Enviar-archivo.php
+++ Parte1/Subir-archivos/Enviar-archivo.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clase 3: Subir Archivos al Servidor con PHP</title>
+    <title>Clase 4: Subir Archivos al Servidor con PHP</title>
 </head>
 <body>
     <form action="Subir-archivo.php" name="enviar_archivo_frm" method="post" enctype="multipart/form-data">
