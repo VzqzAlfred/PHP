fa53107819e12fbfd81a227d7e1c264d075c06cd
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-09-18 18:41:38 -0500

    OTRA FORMA DE CONECTAR BD

---
+++ Parte1/App1-PHP/PHP/Conexion.php
@@ -0,0 +1,20 @@
+<?php       // OTRA FORMA DE CONECTAR BD
+
+    function conectarse(){
+        $servidor="localhost";
+        $usuario="root";
+        $password="";
+        $bd="mis_contactos";
+
+        $conectar = new mysqli($servidor, $usuario, $password, $bd);
+
+        return $conectar;
+    }
+
+    if($conexion = conectarse()){
+        echo "BD conectada";
+    }else{
+        echo "ERROR";
+    }
+
+?>
